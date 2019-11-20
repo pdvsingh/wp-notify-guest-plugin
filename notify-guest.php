0af231e634c8addc3012adc3db82dd752ca4d5e7
psingh <psingh@synack.com>
2019-11-19 21:28:13 -0800

    Basic template

---
+++ notify-guest.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* Plugin Name: Notify Guest on Publish
+* Plugin URI: 
+* Description: This plugin sends a notification to an email when a post is published
+* Version: 0.1
+* Author: Prebh Dev Singh
+* Author URI: https://mainwp.com
+* License: GPL2
+*/
+
+?>
